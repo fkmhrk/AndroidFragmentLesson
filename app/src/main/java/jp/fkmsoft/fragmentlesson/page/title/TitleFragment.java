c69a5060a15cc9c7d055d60599b647b5ed220686
hrk fkm <fkm@fkmsoft.jp>
2015-01-21 16:04:15 +0900

    Add lesson 1 files

---
+++ app/src/main/java/jp/fkmsoft/fragmentlesson/page/title/TitleFragment.java
@@ -0,0 +1,11 @@
+package jp.fkmsoft.fragmentlesson.page.title;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * タイトル画面
+ */
+public class TitleFragment extends Fragment {
+    // Lesson 1. 最低限必要なonCreateView()をオーバーライドし、
+    // R.layout.fragment_titleを表示させてみよう
+}
